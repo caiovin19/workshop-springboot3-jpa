20e96b6ef98c8d6d1487889d6f22562fa8b9d2fa
Caio Vinicius <caio.vin19@outlook.com>
2022-10-21 17:16:58 -0300

    Category

---
+++ src/main/java/com/caio/project/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.caio.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.caio.project.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
