74dee504827ed3b0fc0ed0c273bc135db1ec219d
Caio Vinicius <caio.vin19@outlook.com>
2022-10-24 19:36:54 -0300

    exception

--- src/main/java/com/caio/project/services/UserService.java
+++ src/main/java/com/caio/project/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.caio.project.entities.User;
 import com.caio.project.repositories.UserRepository;
+import com.caio.project.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User>obj=repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
