954793e1f48564c5e31f3fb0919f7d2366382ef5
Caio Vinicius <caio.vin19@outlook.com>
2022-10-24 20:01:14 -0300

    Update Exception

--- src/main/java/com/caio/project/services/UserService.java
+++ src/main/java/com/caio/project/services/UserService.java
@@ -13,6 +13,8 @@ import com.caio.project.repositories.UserRepository;
 import com.caio.project.services.exceptions.DatabaseException;
 import com.caio.project.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,14 @@ public class UserService {
 		}
 	}
 	public User update(Long id, User obj) {
-		User entity=repository.getReferenceById(id);
+		try{
+			User entity=repository.getReferenceById(id);
+		
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
