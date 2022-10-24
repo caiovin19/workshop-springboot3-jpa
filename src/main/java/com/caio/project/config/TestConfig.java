a2cd02ed6552f638cea9aa2a675dfa7f087e8074
Caio Vinicius <caio.vin19@outlook.com>
2022-10-24 16:23:33 -0300

    payment

--- src/main/java/com/caio/project/config/TestConfig.java
+++ src/main/java/com/caio/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.caio.project.entities.Category;
 import com.caio.project.entities.Order;
 import com.caio.project.entities.OrderItem;
+import com.caio.project.entities.Payment;
 import com.caio.project.entities.Product;
 import com.caio.project.entities.User;
 import com.caio.project.entities.enums.OrderStatus;
@@ -85,7 +86,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1=new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
+		orderRepository.save(o1);
 		
 		
 	}
